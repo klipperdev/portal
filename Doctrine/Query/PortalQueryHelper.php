01594d2bd9ec168c006e54c3cfe8642a6d35c61c
François Pluchino <francois.pluchino@klipper.dev>
2021-02-18 13:05:32 +0100

    Always return array for disabled filters in portal helper

--- Doctrine/Query/PortalQueryHelper.php
+++ Doctrine/Query/PortalQueryHelper.php
@@ -153,6 +153,8 @@ class PortalQueryHelper
             } else {
                 $disabledFilters = [];
             }
+        } else {
+            $disabledFilters = [];
         }
 
         return $query;
