34f6dd7e77d91030911b7ca3ba86f3a5d3ccb151
François Pluchino <francois.pluchino@klipper.dev>
2021-08-29 10:53:00 +0200

    Extract isUrlGenerationSafe method in trait

--- Twig/Extension/PortalRoutingExtension.php
+++ Twig/Extension/PortalRoutingExtension.php
@@ -12,10 +12,8 @@
 namespace Klipper\Component\Portal\Twig\Extension;
 
 use Klipper\Component\Portal\Routing\PortalRoutingInterface;
+use Klipper\Component\Routing\Twig\Extension\Traits\UrlGenerationTrait;
 use Twig\Extension\AbstractExtension;
-use Twig\Node\Expression\ArrayExpression;
-use Twig\Node\Expression\ConstantExpression;
-use Twig\Node\Node;
 use Twig\TwigFunction;
 
 /**
@@ -23,6 +21,8 @@ use Twig\TwigFunction;
  */
 class PortalRoutingExtension extends AbstractExtension
 {
+    use UrlGenerationTrait;
+
     private PortalRoutingInterface $portalRouting;
 
     public function __construct(PortalRoutingInterface $portalRouting)
@@ -46,43 +46,5 @@ class PortalRoutingExtension extends AbstractExtension
     public function getPortalPath(string $name, array $parameters = [], bool $relative = false): string
     {
         return $this->portalRouting->getPortalPath($name, $parameters, $relative);
-    }
-
-    /**
-     * Determines at compile time whether the generated URL will be safe and thus
-     * saving the unneeded automatic escaping for performance reasons.
-     *
-     * The URL generation process percent encodes non-alphanumeric characters. So there is no risk
-     * that malicious/invalid characters are part of the URL. The only character within an URL that
-     * must be escaped in html is the ampersand ("&") which separates query params. So we cannot mark
-     * the URL generation as always safe, but only when we are sure there won't be multiple query
-     * params. This is the case when there are none or only one constant parameter given.
-     * E.g. we know beforehand this will be safe:
-     * - portal_path('route')
-     * - portal_path('route', {'param': 'value'})
-     * But the following may not:
-     * - portal_path('route', var)
-     * - portal_path('route', {'param': ['val1', 'val2'] }) // a sub-array
-     * - portal_path('route', {'param1': 'value1', 'param2': 'value2'})
-     * If param1 and param2 reference placeholder in the route, it would still be safe. But we don't know.
-     *
-     * @param Node $argsNode The arguments of the path/url function
-     *
-     * @return array An array with the contexts the URL is safe
-     */
-    public function isUrlGenerationSafe(Node $argsNode): array
-    {
-        // support named arguments
-        $paramsNode = $argsNode->hasNode('parameters') ? $argsNode->getNode('parameters') : (
-            $argsNode->hasNode(1) ? $argsNode->getNode(1) : null
-        );
-
-        if (null === $paramsNode || $paramsNode instanceof ArrayExpression && \count($paramsNode) <= 2
-            && (!$paramsNode->hasNode(1) || $paramsNode->getNode(1) instanceof ConstantExpression)
-        ) {
-            return ['html'];
-        }
-
-        return [];
     }
 }
